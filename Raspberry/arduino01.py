e96aacee5d1a2ca0774dcc34eb130bd78471c470
meccanismocomplesso <meccanismo.complesso@gmail.com>
2018-09-13 09:47:34 +0200

    version 0.0.5

---
+++ Raspberry/arduino01.py
@@ -0,0 +1,8 @@
+import serial
+arduinoSerialData = serial.Serial('/dev/ttyACM0',9600)
+while 1:
+   if(arduinoSerialData.inWaiting() > 0):
+       myData = arduinoSerialData.readline()
+       print myData
+
+
